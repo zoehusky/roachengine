db620c6c14ae9d7d41dd0bdd0ae8e624e8716cce
zoehusky <94269023+zoehusky@users.noreply.github.com>
2021-11-17 00:30:47 -0500

    Create webgl.js

---
+++ utils/webgl.js
@@ -0,0 +1,11 @@
+// library of shaders
+const GL={
+  bfv:[REDACTED],
+  bff:[REDACTED],
+  blv:[REDACTED],
+  blf:[REDACTED],
+  cfv:[REDACTED],
+  cff:[REDACTED],
+  clv:[REDACTED],
+  clf:[REDACTED],
+};
